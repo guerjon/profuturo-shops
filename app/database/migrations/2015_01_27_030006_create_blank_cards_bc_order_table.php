634034b48f49be73dc8dea3b73df97ead751c9e6
Javier E. Lávida <javier.lavida@gmail.com>
2015-01-26 22:52:18 -0600

    [aDDED] Blank Cards

---
+++ app/database/migrations/2015_01_27_030006_create_blank_cards_bc_order_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBlankCardsBcOrderTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+
+		Schema::create('blank_cards_bc_order', function($table)
+		{
+			$table->integer('bc_order_id')->unsigned();
+			$table->integer('quantity');
+			$table->tinyInteger('status')->default(0);
+			$table->string('comments');
+			$table->foreign('bc_order_id')->references('id')->on('bc_orders')->onDelete('cascade');
+		});
+
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('blank_cards_bc_order', function($table){
+			$table->dropForeign('blank_cards_bc_order_bc_order_id_foreign');
+		});
+		Schema::drop('blank_cards_bc_order');
+	}
+
+}
