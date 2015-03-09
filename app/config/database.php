aafdd92e3dc070bf77734e50c228510188d76052
Nancy Mejía <nanmejia@Mac-mini-de-Ivan.local>
2015-03-09 14:53:38 -0600

    configuracion base

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'profcom_store',
-			'username'  => 'profcom_store',
-			'password'  => 'profcom_store@1',
+			'database'  => 'appssori_profuturo_store',
+			'username'  => 'appssori_pfstore',
+			'password'  => 'Pfstore@1',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
