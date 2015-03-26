38a04c421bbd88441b7d0901a5dec9c141a3c54e
Jonathan Guerrero <jonguer@iMac-de-Ivan.local>
2015-03-26 14:43:56 -0600

    no hay cambios

--- app/models/GeneralRequest.php
+++ app/models/GeneralRequest.php
@@ -7,6 +7,8 @@ class GeneralRequest extends Eloquent
 
   protected $dates = ['created_at', 'updated_at', 'project_date'];
 
+  protected $appends = ['project_date_formatted', 'total'];
+
   public function user(){
     return $this->belongsTo('User');
   }
@@ -14,4 +16,15 @@ class GeneralRequest extends Eloquent
   public function manager(){
     return $this->belongsTo('User', 'manager_id');
   }
+
+  public function getProjectDateFormattedAttribute()
+  {
+  	return $this->project_date->format('Y-m-d');
+  }
+
+  public function getTotalAttribute()
+  {
+  	return $this->quantity*$this->unit_price;
+  }
+
 }
