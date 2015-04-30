ad272ce6cf513c72d77f5f31fb3b77054e7f2ea9
jonguer <jona_54_.com@hotmail.com>
2015-04-30 17:50:36 -0500

    se terminaron las secciones de reportes

---
+++ app/admin_views/reports/total_user_orders.blade.php
@@ -0,0 +1,98 @@
+@extends('layouts.master')
+
+@section('content')
+
+<div class="page-header">
+  <h3>Reporte total de usuarios</h3>
+</div>
+
+{{Form::open([
+  'id' => 'filter-form',
+  'method' => 'GET',
+  'action' => 'AdminApiController@getTotalUsersExcel',
+  'target' => '_blank'
+  ])}}
+<div class="row">
+ 
+  <center>
+      {{ Form::submit('Descargar excel', ['class' => 'btn btn-warning btn-submit'])}}
+  </center>
+  
+
+</div>
+{{Form::close()}}
+
+<hr>
+
+<div class="table-responsive">
+  <table class="table table-responsive">
+    <thead>
+      <tr>
+
+      </tr>
+    </thead>
+    <tbody>
+
+    </tbody>
+  </table>
+</div>
+
+@stop
+
+@section('script')
+<script>
+
+function update(){
+  $('.table tbody').empty();
+  $('.table tbody').append(
+    $('<tr>').attr('class', 'info').append(
+      $('<td>').attr('colspan', $('.table thead tr:first-child th').length).html('<strong>Cargando...</strong>')
+    )
+  );
+  $.get('/admin/api/total-users-report', $('#filter-form').serialize(), function(data){
+    $('.table tbody').empty();
+    if(data.status == 200){
+      var orders = data.users;
+      var headers = ['ID', 'CENTRO_DE_COSTOS', 'GERENCIA','LINEA_NEGOCIO','PEDIDOS','COMPLETOS','INCOMPLETOS','PENDIENTES'];
+      $('.table thead tr').empty();
+      if(orders.length == 0){
+        $('.table tbody').append(
+          $('<tr>').attr('class', 'warning').append(
+            $('<td>').html('<strong>No hay registros que mostrar</strong>')
+          )
+        );
+        $('.btn-submit').prop('disabled', true);
+        return;
+      }else{
+        $('.btn-submit').prop('disabled', false);
+      }
+
+      for(var i=0; i<headers.length; i++){
+        $('.table thead tr').append($('<th>').html(headers[i]));
+      }
+      headers = ['id','ccosto','gerencia','linea_negocio','pedidos','completos','incompletos','pendientes'];
+      for(var i=0; i<orders.length; i++){
+        var tr = $('<tr>');
+
+        for(var j=0; j<headers.length; j++){
+          tr.append($('<td>').html(orders[i][headers[j]]));
+        }
+        $('.table tbody').append(tr);
+      }
+    }else{
+      $('.table tbody').append(
+        $('<tr>').attr('class', 'danger').append(
+          $('<td>').attr('colspan', $('.table > thead > tr th').length).html(data.status + ':' + data.error_msg)
+        )
+      );
+    }
+  });
+}
+$(function(){
+  update();
+  $('#filter-form select').change(function(){
+    update();
+  });
+});
+</script>
+@stop
