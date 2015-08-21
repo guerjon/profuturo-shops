5d0bc903f6282890a3f44a6b68b6712f904fcb5b
jonguer <jona_54_.com@hotmail.com>
2015-08-21 16:50:29 -0500

    [FIXED] cambio en la imagen para miniatura

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -58,7 +58,7 @@
               @if(Auth::check())
               <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                  {{HTML::image(Auth::user()->image->url('mini'), Auth::user()->nombre, ['class' => 'img-rounded'])}}
+                  {{HTML::image(Auth::user()->image->url('mini'), Auth::user()->nombre, ['class' => 'img-rounded','style' => ' height: 30px;width: 30px;'] )}}
                   {{Auth::user()->nombre}}
                   <span class="caret"></span>
                 </a>
