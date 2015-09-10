41ce2b3de412395ea07e209bf48cda3d0fb664d7
jonguer <jona_54_.com@hotmail.com>
2015-09-10 17:54:33 -0500

    [ADDED] checkbox para mostrar solo los campos que se deesen

--- app/admin_views/reports/bi.blade.php
+++ app/admin_views/reports/bi.blade.php
@@ -6,6 +6,7 @@
   <h3>Reporte de BI</h3>
 </div>
 
+
 {{Form::open([
   'id' => 'filter-form',
   'method' => 'GET',
@@ -16,26 +17,8 @@
   {{-- <div class="col-xs-2 col-xs-offset-2">
     {{Form::select('region_id',$regions,null,['class' => 'form-control'])}}
   </div> --}}
-  <div class="col-xs-2">
-    {{Form::label('order_id','# Pedido')}}
-    {{Form::text('order_id',null,['class' => 'form-control','placeholder' => 'Ingrese el numero de pedido','id' => 'order'])}}
-  </div>
 
-  <div class="col-xs-2">
-    {{Form::label('CCOSTO','Ccosto')}}
-    {{Form::text('ccosto',null,['class' => 'form-control','placeholder' => 'Ingrese un ccosto','id' => 'ccosto'])}}
-  </div>
-  
-  <div class="col-xs-2">
-    {{Form::label('category_id','Categoria')}}
-    {{Form::select('category_id',[null => 'Seleccione una categoria'] + $categories,null,['class' => 'form-control'])}}
-  </div>
-
-  <div class="col-xs-2">
-    {{Form::label('product_id','Producto')}}
-    {{Form::select('product_id',[null => 'Seleccione un producto'] +$products,null,['class' => 'form-control'])}}
-  </div>
-  
+    
   <div class="col-xs-1">
     {{Form::label('since','Desde')}}
     {{Form::text('since',\Carbon\Carbon::now('America/Mexico_City')->subMonths(1)->format('Y-m-d'), ['class' => 'form-control datepicker','id' => 'since' ])}}
@@ -45,10 +28,39 @@
     {{Form::label('until','Hasta')}}
     {{Form::text('until',\Carbon\Carbon::now('America/Mexico_City')->format('Y-m-d'), ['class' => 'form-control datepicker','id' => 'until' ])}}
   </div>
+
+  <div class="col-xs-2">
+     <label for="order">
+        <input type="checkbox" class="checkbox-filter" data-filter="order"> #Numero de pedido 
+      </label>
+    {{Form::text('order_id',null,['class' => 'form-control','placeholder' => 'Ingrese el numero de pedido','id' => 'order','style' => 'display:none' ])}}
+  </div>
+
+  <div class="col-xs-2">
+    <label for="order">
+      <input type="checkbox" class="checkbox-filter" data-filter="ccosto"> Ccosto 
+    </label>  
+    {{Form::text('ccosto',null,['class' => 'form-control','placeholder' => 'Ingrese un ccosto','id' => 'ccosto','style' => 'display:none'])}}
+  </div>
+  
+  <div class="col-xs-2">
+    <label for="order">
+      <input type="checkbox" class="checkbox-filter" data-filter="category_id"> Categoria  
+    </label>   
+    {{Form::select('category_id',[null => 'Seleccione una categoria'] + $categories,null,['class' => 'form-control','id' => 'category_id','style' => 'display:none'])}}
+  </div>
+
+  <div class="col-xs-2">
+    <label for="order">
+      <input type="checkbox" class="checkbox-filter" data-filter="product_id"> Producto
+    </label>
+    {{Form::select('product_id',[null => 'Seleccione un producto'] +$products,null,['class' => 'form-control','id' => 'product_id','style' => 'display:none'])}}
+  </div>
+
   
   <div class="col-xs-1">
     
-    {{ Form::submit('Descargar excel', ['class' => 'btn btn-warning btn-submit'])  }}
+    {{ Form::submit('Excel', ['class' => 'btn btn-warning btn-submit'])  }}
   </div>
 
   <div class="col-xs-1">
@@ -63,10 +75,10 @@
 
       <!-- Modal para la gráfica-->
   <div id="graph" class="modal fade " role="dialog">
-    <div class="modal-dialog  modal-lg">
+    <div class="modal-dialog  modal-lg" style="width:70%">
 
       <!-- Modal content-->
-      <div class="modal-content">
+      <div class="modal-content" >
         <div class="modal-header">
           <button type="button" class="close" data-dismiss="modal">&times;</button>
           <h4 class="modal-title">Gráfica</h4>
@@ -74,8 +86,10 @@
         <div class="modal-body">
           
         
-
+      <center>
         <div id="chart_div"></div>
+      </center>
+        
 
         <div class="form-group">
           <center>
@@ -119,11 +133,10 @@
 
 
 function drawChart(datos,tipo) {
-        console.log(datos);
-        console.log(tipo);
+
         var title = '';
         var columns = [[]];
-
+        chart = new google.visualization.PieChart(document.getElementById('chart_div'));
         if(tipo == 'orders_category') 
         {  
           title = 'Pedidos por categoría';
@@ -131,6 +144,7 @@ function drawChart(datos,tipo) {
           for(var i = 0;i < datos.orders_by_category.length;i++){
             columns.push(datos.orders_by_category[i]);
           };
+          chart = new google.visualization.PieChart(document.getElementById('chart_div'));
         }  
 
         if(tipo == 'orders_region') 
@@ -140,6 +154,7 @@ function drawChart(datos,tipo) {
           for(var i = 0;i < datos.orders_by_region.length;i++){
             columns.push(datos.orders_by_region[i]);
           };
+           chart = new google.visualization.PieChart(document.getElementById('chart_div'));
         };
 
         if(tipo == 'expensives_region') 
@@ -149,6 +164,7 @@ function drawChart(datos,tipo) {
           for(var i = 0;i < datos.expenses_by_region.length;i++){
             columns.push(datos.expenses_by_region[i]);
           };
+           chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
         };
 
 
@@ -159,12 +175,12 @@ function drawChart(datos,tipo) {
 
         // Set chart options
         var options = {'title':title,
-                       'width':700,
-                       'height':350,
+                       'width': 650,
+                       'height': 550,
                        is3D: true};
 
         // Instantiate and draw our chart, passing in some options.
-        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+        
         
         chart.draw(data, options);
 } 
@@ -234,8 +250,18 @@ function update(){
 
 $(function(){
   google.load('visualization', '1', {'packages':['corechart'], "callback": drawChart});
-   update();
+  update();
 
+  $('.checkbox-filter').change(function(){
+      var campo = $(this).attr('data-filter');
+      if($(this).is(':checked')){
+        $('#'+campo).css('display','block'); 
+      }else{
+        $('#'+campo).css('display','none');  
+      }
+      
+      
+  });
 
   $('#filter-form select').change(function(){
      update();
@@ -243,9 +269,7 @@ $(function(){
 
 
   $('#order').keyup(function(){
-
      update();
-
   });
 
   $('#ccosto').keyup(function(){
@@ -254,12 +278,10 @@ $(function(){
 
   $('#until').change(function(){
       update();
-
   });  
 
   $('#since').change(function(){
       update();
-
   });  
 
   $.ajax({
@@ -271,7 +293,6 @@ $(function(){
              
               var orders = data.orders;
               var ccostos = data.ccostos;
-
 
                // $('#order').autocomplete(
                //   { 
