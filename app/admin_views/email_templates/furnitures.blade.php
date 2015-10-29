7e1d7bcb5dd543ba542bb733648a9951e8758fce
jonguer <jona_54_.com@hotmail.com>
2015-10-29 17:01:31 -0600

    [ADDED]

--- app/admin_views/email_templates/furnitures.blade.php
+++ app/admin_views/email_templates/furnitures.blade.php
@@ -5,9 +5,11 @@
 			Numero de orden: {{$order->id}}
 			<br>
 			@foreach($order->products as $product) 
+     		<br>
      		Producto: {{$product->name}}
      		<br>
      		Cantidad:{{$product->pivot->quantity}}
+				<br>
 			@endforeach
 		
 		</div>
