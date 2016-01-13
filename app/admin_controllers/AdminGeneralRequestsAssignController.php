ac2c9d588b7f0540589e2729150a9611c244c02a
jonguer <jona_54_.com@hotmail.com>
2016-01-13 16:59:30 -0600

    [test] correos probados

--- app/admin_controllers/AdminGeneralRequestsAssignController.php
+++ app/admin_controllers/AdminGeneralRequestsAssignController.php
@@ -25,7 +25,7 @@ class AdminGeneralRequestsAssignController extends AdminBaseController{
       $message->to($email)->subject("Solicitud general asignada.");
     });
     //$email_mary = 'carmen.fernandez@profuturo.com.mx';
-
+    $email = $manager->email;
     Mail::send('admin::email_templates.aviso_consultor',['general_request' => $request],function($message) use ($email){
       $message->to($email)->subject("Solicitud general asignada por Administrador.");
     });
