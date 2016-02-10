5d008883dc0ed9d388e5c690a4f566ae2e71fd8d
jonguer <jona_54_.com@hotmail.com>
2016-02-10 16:43:26 -0600

    añadido boton para cambiar de domicilio en caso de que así lo requiera el pedido

--- app/admin_views/orders/index.blade.php
+++ app/admin_views/orders/index.blade.php
@@ -11,130 +11,162 @@
     <li class="active">Pedidos Papelería</li>
   </ol>
 
-@if($orders->count() == 0)
-  <div class="alert alert-info">
-    No se han realizado pedidos todavía.
-  </div>
-@else
-  <a href="{{action('AdminOrdersController@index', ['export'=>'xls'])}}" class="btn btn-primary btn-submit" style="float:right">
-    <span class="glyphicon glyphicon-download-alt"></span> Descargar excel
-  </a>
-
-  {{Form::open([
-    'method' => 'GET',
-    'class' => 'form-inline'
-    ])}}
-
-    <div class="form-group">
-      {{Form::number('ccosto', Input::get('ccosto'), ['class' => 'form-control', 'placeholder' => 'CCOSTOS'])}}
+  @if($orders->count() == 0)
+    <div class="alert alert-info">
+      No se han realizado pedidos todavía.
     </div>
+  @else
+    <a href="{{action('AdminOrdersController@index', ['export'=>'xls'])}}" class="btn btn-primary btn-submit" style="float:right">
+      <span class="glyphicon glyphicon-download-alt"></span> Descargar excel
+    </a>
 
-    <div class="form-group">
-      {{Form::select('gerencia', [NULL => 'Todas las gerencias'] + $gerencias, Input::get('gerencia'), ['class' => 'form-control'])}}
-    </div>
+    {{Form::open([
+      'method' => 'GET',
+      'class' => 'form-inline'
+      ])}}
 
-    <div class="form-group">
-      {{Form::select('divisional_id', [NULL => 'Todas las divisionales'] + Divisional::orderBy('id')->lists('name','id'), Input::get('gerencia'), ['class' => 'form-control'])}}
-    </div>
+      <div class="form-group">
+        {{Form::number('ccosto', Input::get('ccosto'), ['class' => 'form-control', 'placeholder' => 'CCOSTOS'])}}
+      </div>
 
-    <div class="form-group">
-      <button type="submit" class="btn btn-primary">
-        <span class="glyphicon glyphicon-filter"></span> Filtrar
-      </button>
-    </div>
+      <div class="form-group">
+        {{Form::select('gerencia', [NULL => 'Todas las gerencias'] + $gerencias, Input::get('gerencia'), ['class' => 'form-control'])}}
+      </div>
 
-  {{Form::close()}}
-  
-  <div class="container-fluid">
-    <table class="table table-striped">
-      <thead>
-        <tr>
+      <div class="form-group">
+        {{Form::select('divisional_id', [NULL => 'Todas las divisionales'] + Divisional::orderBy('id')->lists('name','id'), Input::get('gerencia'), ['class' => 'form-control'])}}
+      </div>
 
-           <th>
-          Gerencia
-          </th>
-          <th>
-            Centro de costos
-          </th>
-          <th>
-            No. pedido
-          </th>
+      <div class="form-group">
+        <button type="submit" class="btn btn-primary">
+          <span class="glyphicon glyphicon-filter"></span> Filtrar
+        </button>
+      </div>
 
-          <th>
-            Comentarios
-          </th>
-          <th>
-            Fecha de pedido
-          </th>
-          <th>
-            Estatus
-          </th>
-          <th>
-            Domicilio
-          </th>
-          <th>
-           Acciones
-          </th>
-          
-        </tr>
-      </thead>
-
-      <tbody>
-        @foreach($orders as $order)
+    {{Form::close()}}
+    
+    <div class="container-fluid">
+      <table class="table table-striped">
+        <thead>
           <tr>
-            <td>
-              {{$order->user->gerencia}}
-            </td>
-            <td>
-              {{$order->user->ccosto}}
-            </td>
-            <td>
-              {{link_to_action('AdminOrdersController@show', $order->id, [$order->id])}}
-            </td>
-            <td>
-              {{$order->comments}}
-            </td>
-            <td>
-              {{$order->created_at->format('d-m-Y')}}
-            </td>
-            <td>
-              @if($order->status == 0)
-                Pendiente
-              @elseif($order->status==1)
-                Recibido <span class="glyphicon glyphicon-check"></span>
-              @elseif($order->status==2)
-                Recibido incompleto.
-                @if($complain = $order->order_complain)
-                  <small>{{$complain->complain}}</small>
+
+             <th>
+            Gerencia
+            </th>
+            <th>
+              Centro de costos
+            </th>
+            <th>
+              No. pedido
+            </th>
+
+            <th>
+              Comentarios
+            </th>
+            <th>
+              Fecha de pedido
+            </th>
+            <th>
+              Estatus
+            </th>
+            <th>
+              Domicilio
+            </th>
+            <th>
+             Acciones
+            </th>
+            
+          </tr>
+        </thead>
+
+        <tbody>
+          @foreach($orders as $order)
+            <tr>
+              <td>
+                {{$order->user->gerencia}}
+              </td>
+              <td>
+                {{$order->user->ccosto}}
+              </td>
+              <td>
+                {{link_to_action('AdminOrdersController@show', $order->id, [$order->id])}}
+              </td>
+              <td>
+                {{$order->comments}}
+              </td>
+              <td>
+                {{$order->created_at->format('d-m-Y')}}
+              </td>
+              <td>
+                @if($order->status == 0)
+                  Pendiente
+                @elseif($order->status==1)
+                  Recibido <span class="glyphicon glyphicon-check"></span>
+                @elseif($order->status==2)
+                  Recibido incompleto.
+                  @if($complain = $order->order_complain)
+                    <small>{{$complain->complain}}</small>
+                  @endif
                 @endif
-              @endif
-            </td>
-            <td>
-              @foreach($addresses as $address)
-                @if($address->ccostos == $order->user->ccosto)
-                    {{$address->domicilio}}
-                    @if($address->posible_cambio != null)
-                        Cambio domicilio
-                    @endif
-                @endif
-              @endforeach
-            </td>
-            <td>
-              {{Form::open(array('action' =>['AdminOrdersController@destroy',$order->id],
-              'method' => 'delete'))}}
-                <button type="submit" class="btn btn-danger btn-xs">
-                  <span class="glyphicon glyphicon-remove"></span> Eliminar
-                </button>
-               {{Form::close()}}
-            </td>
-            </tr>
-          @endforeach
-        </tbody>
-    </table>
-  </div>
-  <center>
-    {{$orders->links()}}
-  </center>
-@endif
+              </td>
+              <td>
+                @foreach($addresses as $address)
+                  @if($address->ccostos == $order->user->ccosto)
+                      {{$address->domicilio}}
+                      @if($address->posible_cambio != null)
+                      <button data-id="{{$address->id}}" data-domicilio="{{$address->domicilio}}" data-posible-cambio="{{$address->posible_cambio}}" class="btn btn-primary btn-xs" id="cambio">
+                        Ver cambio domicilio
+                      </button>
+                          
+                      @endif
+                  @endif
+                @endforeach
+              </td>
+              <td>
+                {{Form::open(array('action' =>['AdminOrdersController@destroy',$order->id],
+                'method' => 'delete'))}}
+                  <button type="submit" class="btn btn-danger btn-xs">
+                    <span class="glyphicon glyphicon-remove"></span> Eliminar
+                  </button>
+                 {{Form::close()}}
+              </td>
+              </tr>
+            @endforeach
+          </tbody>
+      </table>
+    </div>
+    <center>
+      {{$orders->links()}}
+    </center>
+  @endif
+  @include('admin::orders.partials.change_address')
+
 @stop
 
+@section('script')
+  <script>
+    $(function(){
+      $('#cambio').click(function(){
+          var modal = $('#change-address').modal();
+
+          $('#domicilio').text($(this).attr('data-domicilio'));
+          $('#posible_cambio').text($(this).attr('data-posible-cambio'));
+          var action = 'address/'+ $(this).attr('data-id');
+          $('#change-address-form').attr('action',action);          
+          modal.show();
+      });
+
+
+      $('.approve').click(function(){
+        console.log($(this).attr('data-value'));
+        if($(this).attr('data-value') == 1)
+          $('#valor_aprobado').val(1);
+        else
+          $('#valor_aprobado').val(0);
+
+        $('#change-address-form').submit();
+      });
+    });
+  </script>
+
+@endsection
