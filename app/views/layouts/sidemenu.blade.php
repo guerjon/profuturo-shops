a23f5783ab7c6546c994f2622c3027e3ea757e21
jonguer <jona_54_.com@hotmail.com>
2016-03-16 12:10:37 -0600

    sublsitas anadidas

--- app/views/layouts/sidemenu.blade.php
+++ app/views/layouts/sidemenu.blade.php
@@ -4,9 +4,109 @@
   @else
 
   @foreach(Auth::user()->menu_actions as $action => $name)
-  <li role="presentation">{{link_to($action, $name)}}</li>
-  @endforeach
+  	@if($name == "Categorías")	
+		<li class="" >
+			<a href="#"  class="submenu-dispatch accordion-toggle"
+			  data-toggle="collapse" data-parent="#menu-content" data-target="#tipo-categoria">
+			    Categorías
+			</a>
+		<ul id="tipo-categoria" class="sub-menu collapse" >
+			<li class="">
+				<a href="{{action('AdminCategoriesController@index')}}">
+					Categorías Productos
+				</a>
+			</li>
+			<li class="">
+				<a href="{{action('AdminFurnitureCategoriesController@index')}}">
+					
+					Categorías Mobilario
+				</a>
+			</li>
+			<li class="">
+				<a href="{{action('AdminMacCategoriesController@index')}}">
+					
+					Categorías MAC
+				</a>
+			</li>
+		</ul>
+	</li>
+	@elseif($name == "Productos")
+		<li class=" {{ substr(Route::currentRouteName(), 0, 5)=='tabla'?'active':''}}">
+				<a href="#"  class="submenu-dispatch accordion-toggle"
+				  data-toggle="collapse" data-parent="#menu-content" data-target="#tipo-producto">
+				    Productos
+				</a>
+			<ul id="tipo-producto" class="sub-menu collapse" >
+				<li class="">
+					<a href="{{action('AdminProductsController@index')}}">
+						
+						Productos Papelería
+					</a>
+				</li>
+				<li class="">
+					<a href="{{action('AdminBusinessCardsController@index')}}">
+						
+						Productos Tarjetas
+					</a>
+				</li>
+				<li class="">
+					<a href="{{action('AdminFurnituresController@index')}}">
+						
+						Productos Muebles
+					</a>
+				</li>
+				<li class="">
+					<a href="{{action('AdminMacProductsController@index')}}">
+						
+						Productos MAC
+					</a>
+				</li>
+			</ul>
+		</li>
+	@elseif($name == "Pedidos")
+		<li class="">
+				<a href="#"  class="submenu-dispatch accordion-toggle"
+				  data-toggle="collapse" data-parent="#menu-content" data-target="#tipo-pedido">
+				    Pedidos
+				</a>
+			<ul id="tipo-pedido" class="sub-menu collapse" >
+				
+					<li class="">		
+						<a href="{{action('AdminOrdersController@index')}}">
+							Pedidos papelería
+						</a>	
+					</li>
+				
 
+				<li class="">
+					<a href="{{action('AdminBcOrdersController@index')}}">
+						
+						Pedidos tarjetas
+					</a>
+				</li>
+				<li class="">
+					<a href="{{action('AdminFurnituresOrdersController@index')}}">
+						
+						Pedidos muebles
+					</a>
+				</li>
+				<li class="">
+					<a href="{{action('AdminMacOrdersController@index')}}">
+						
+						Pedidos mac
+					</a>
+				</li>
+			</ul>
+		</li>
+	
+
+	@else
+		<li role="presentation">{{link_to($action, $name)}}</li>
+	@endif
+  
+
+  @endforeach
+   
   @endif
 
 </ul>
