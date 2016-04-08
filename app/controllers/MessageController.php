2faf0e3ea70c49e232f86750baa4c554a8934cd4
jonguer <jona_54_.com@hotmail.com>
2016-04-08 15:38:17 -0500

    quedaron los mensajes

--- app/controllers/MessageController.php
+++ app/controllers/MessageController.php
@@ -12,16 +12,30 @@ class MessageController extends \BaseController {
 
 		switch ($type) {
 			case 'user':
-					$users = Input::get('users');
-					$this->sendUserMessage($users,$body);
+					
+				if($this->sendUserMessage(Input::get('users'),$body))
+					return Redirect::back()->withSuccess('El mensaje fue enviado con exito');
+				else
+					return Redirect::back()->withErrors("Algo salio mal");
+
 				break;
+
 			case 'divisional':
-					$divisionals = Input::get('divisionals');
-					$this->sendDivisionalMessage($divisionals,$body);
+
+				if($this->sendDivisionalMessage(Input::get('divisionals'),$body))
+					return Redirect::back()->withSuccess('El mensaje fue enviado con exito');
+				else
+					return Redirect::back()->withErrors("Algo salio mal");
+				
 				break;
+
 			case 'region':
-					$region = Input::get('regions');
-					$this->sendRegionMessage($region,$body);
+
+				if($this->sendRegionMessage(Input::get('regions'),$body))
+					return Redirect::back()->withSuccess('El mensaje fue enviado con exito');
+				else
+					return Redirect::back()->withErrors("Algo salio mal");
+					
 				break;
 		}
 	}
@@ -43,87 +57,65 @@ class MessageController extends \BaseController {
 					$user->messages()->attach($message->id);
 				}
 			}else{
-				return Redirect::back()->withErrors('No se encontro el usuario.');
+				return false;
 			}
 		}
 
-		
-			return Redirect::back()->withSuccess('El mensaje fue enviado con exito');
-		
+		return true;		
 	}
 
 	private function sendDivisionalMessage($divisionals,$body)
 	{
 
-		foreach ($divisionals as $divisional_id) {
-			$divisional = Divisional::find($divisional_id);
-			
-			if(!$divisional)
-				return Redirect::back()->withErrors("No se encontro la region");
+		foreach ($divisionals as $divisional) {
 
-			$users = $divisional->users;
+			$divisional = Divisional::find($region_id);
 			
-			foreach ($users as $user_id) {
+			if(!$region)
+				
+			$users = DB::table('users')->where('region_id',$region->id)->get();
 
-				$user = User::find($user_id);
+			foreach ($users as $user) {
+				
 				if($user){
-					$values = [ 'sender_id' => Auth::user()->id,
-		                'receiver_id' => $user->id,
-		                'body' => $body,
-		                'type' => 'personal'
-	              	];
-
-					$message = new Message($values);
-
-					if($message->save()){
-						$user->messages()->attach($message->id);
-					}
+					
+					$message = new Message();
+					$message->sendMessage(Auth::user()->id,$user->id,$body);
+					
 				}else{
-					return Redirect::back()->withErrors('No se encontro el usuario.');
+					return false;
 				}
 			}
 
-			return Redirect::back()->withSuccess('El mensaje fue enviado con exito');
 			
-		}		
+		}	
+		return true;
 	}
 
-	private function sendRegionMessage($regions,$body){
+	private function sendRegionMessage($regions,$body)
+	{
 		foreach ($regions as $region_id) {
 
 			$region = Region::find($region_id);
-			
+
 			if(!$region)
-				return Redirect::back()->withErrors("No se encontro la region");
+				return false;
 			
-			$users = $region->users;
-			Log::debug('?????????????????????????????');
-			Log::debug($users);
-			Log::debug('?????????????????????????????');
+			$users = DB::table('users')->where('region_id',$region->id)->get();
 
-			foreach ($users as $id) {
-				$user = User::find($id);
+			foreach ($users as $user) {
+				
 				if($user){
-					$values = [ 'sender_id' => Auth::user()->id,
-		                'receiver_id' => $user->id,
-		                'body' => $body,
-		                'type' => 'personal'
-	              	];
-
-					$message = new Message($values);
-
-					if($message->save()){
-						$user->messages()->attach($message->id);
-						$count++;
-					}
+					
+					$message = new Message();
+					$message->sendMessage(Auth::user()->id,$user->id,$body);
+					
 				}else{
-					return Redirect::back()->withErrors('No se encontro el usuario.');
+					return false;
 				}
 			}
-
-			return Redirect::back()->withSuccess('El mensaje fue enviado con exito');
-			
 		}
+		return true;
 	}
 
 }
