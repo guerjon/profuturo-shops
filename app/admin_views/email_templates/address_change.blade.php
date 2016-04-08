6219a27be74ef963f081f8e6dbd02aab8dfbd9ed
jonguer <jona_54_.com@hotmail.com>
2016-04-08 13:55:59 -0500

    se agrego envio de correo al hacer un cambio de domicilio

---
+++ app/admin_views/email_templates/address_change.blade.php
@@ -0,0 +1,17 @@
+ <html>
+ 	<body>
+
+	<h1>Se solicita un cambio de domicilio para el usuario {{$user->gerencia}} con CCOSTOS {{$user->ccosto}}</h1>	
+		<br>
+		<p>El domicilio actual del usuario es </p>
+		<br>
+		<h3>{{$address->domicilio}}</h3>
+		<br>
+		Y se sugiere que la dirección correcta es esta.
+		<br>
+		<h3>{{$address->posible_cambio}}</h3>
+		<br>
+		Para aprobar este cambio de click aquí 
+		http://store.profuturocompras.com.mx/admin/users?active_tab=user_mac&user_mac%5Bemployee_number%5D={{$user->ccosto}}&user_mac%5Bgerencia%5D=
+ 	</body>
+ </html>
