be7b701cae7341750a0419b76b0dd04733e53c73
Edgard Morales <3ddmoo@gmail.com>
2016-05-27 17:05:24 -0500

    [FIXED] estilos

--- app/admin_views/orders/index.blade.php
+++ app/admin_views/orders/index.blade.php
@@ -2,169 +2,169 @@
 
 @section('content')
 
-  <ol class="breadcrumb">
-    <a href="{{URL::previous()}}" class="back-btn">
-      <span class="glyphicon glyphicon-arrow-left"></span> Regresar
-    </a>
-      &nbsp;&nbsp;&nbsp;
-    <li><a href="/">Inicio</a></li>
-    <li class="active">Pedidos Papelería</li>
-  </ol>
+<ol class="breadcrumb">
+	<a href="{{URL::previous()}}" class="back-btn">
+		<span class="glyphicon glyphicon-arrow-left"></span> Regresar
+	</a>
+	&nbsp;&nbsp;&nbsp;
+	<li><a href="/">Inicio</a></li>
+	<li class="active">Pedidos Papelería</li>
+</ol>
 
-  @if($orders->count() == 0)
-    <div class="alert alert-info">
-      No se han realizado pedidos todavía.
-    </div>
-  @else
-    <a href="{{action('AdminOrdersController@index', ['export'=>'xls'])}}" class="btn btn-primary btn-submit" style="float:right">
-      <span class="glyphicon glyphicon-download-alt"></span> Descargar excel
-    </a>
+@if($orders->count() == 0)
+<div class="alert alert-info">
+	No se han realizado pedidos todavía.
+</div>
+@else
+<a href="{{action('AdminOrdersController@index', ['export'=>'xls'])}}" class="btn btn-primary btn-submit" style="float:right">
+	<span class="glyphicon glyphicon-download-alt"></span> Descargar excel
+</a>
 
-    {{Form::open([
-      'method' => 'GET',
-      'class' => 'form-inline'
-      ])}}
+{{Form::open([
+	'method' => 'GET',
+	'class' => 'form-inline'
+	])}}
 
-      <div class="form-group">
-        {{Form::number('ccosto', Input::get('ccosto'), ['class' => 'form-control', 'placeholder' => 'CCOSTOS'])}}
-      </div>
+	<div class="form-group">
+		{{Form::number('ccosto', Input::get('ccosto'), ['class' => 'form-control', 'placeholder' => 'CCOSTOS'])}}
+	</div>
 
-      <div class="form-group">
-        {{Form::select('gerencia', [NULL => 'Todas las gerencias'] + $gerencias, Input::get('gerencia'), ['class' => 'form-control'])}}
-      </div>
+	<div class="form-group">
+		{{Form::select('gerencia', [NULL => 'Todas las gerencias'] + $gerencias, Input::get('gerencia'), ['class' => 'form-control'])}}
+	</div>
 
-      <div class="form-group">
-        {{Form::select('divisional_id', [NULL => 'Todas las divisionales'] + Divisional::orderBy('id')->lists('name','id'), Input::get('gerencia'), ['class' => 'form-control'])}}
-      </div>
+	<div class="form-group">
+		{{Form::select('divisional_id', [NULL => 'Todas las divisionales'] + Divisional::orderBy('id')->lists('name','id'), Input::get('gerencia'), ['class' => 'form-control'])}}
+	</div>
 
-      <div class="form-group">
-        <button type="submit" class="btn btn-primary">
-          <span class="glyphicon glyphicon-filter"></span> Filtrar
-        </button>
-      </div>
+	<div class="form-group">
+		<button type="submit" class="btn btn-primary">
+			<span class="glyphicon glyphicon-filter"></span> Filtrar
+		</button>
+	</div>
 
-    {{Form::close()}}
-    
-    <div class="container-fluid">
-      <table class="table table-striped">
-        <thead>
-          <tr>
+	{{Form::close()}}
+	
+	<div class="container-fluid mt-30">
+		<table class="table table-striped">
+			<thead>
+				<tr>
 
-             <th>
-            Gerencia
-            </th>
-            <th>
-              Centro de costos
-            </th>
-            <th>
-              No. pedido
-            </th>
+					<th>
+						Gerencia
+					</th>
+					<th>
+						Centro de costos
+					</th>
+					<th>
+						No. pedido
+					</th>
 
-            <th>
-              Comentarios
-            </th>
-            <th>
-              Fecha de pedido
-            </th>
-            <th>
-              Estatus
-            </th>
-            <th>
-              Domicilio
-            </th>
-            <th>
-             Acciones
-            </th>
-            
-          </tr>
-        </thead>
+					<th>
+						Comentarios
+					</th>
+					<th>
+						Fecha de pedido
+					</th>
+					<th>
+						Estatus
+					</th>
+					<th>
+						Domicilio
+					</th>
+					<th>
+						Acciones
+					</th>
 
-        <tbody>
-          @foreach($orders as $order)
-            <tr>
-              <td>
-                {{$order->user->gerencia}}
-              </td>
-              <td>
-                {{$order->user->ccosto}}
-              </td>
-              <td>
-                {{link_to_action('AdminOrdersController@show', $order->order_id, [$order->order_id])}}
-              </td>
-              <td>
-                {{$order->comments}}
-              </td>
-              <td>
-                {{$order->order_date}}
-              </td>
-              <td>
-                @if($order->status == 0)
-                  Pendiente
-                @elseif($order->status==1)
-                  Recibido <span class="glyphicon glyphicon-check"></span>
-                @elseif($order->status==2)
-                  Recibido incompleto.
-                  @if($complain = $order->order_complain)
-                    <small>{{$complain->complain}}</small>
-                  @endif
-                @endif
-              </td>
+				</tr>
+			</thead>
 
-              <td>
-                
-                {{$order->user->address ? $order->user->address->domicilio : "N/A"}}
-                @if($order->user->address ? ($order->user->address->posible_cambio != null) : false )
-                  <button data-id="{{$order->user->address->id}}" data-domicilio="{{$order->user->address->domicilio}}" data-posible-cambio="{{$order->user->address->posible_cambio}}" class="btn btn-primary btn-xs" id="cambio">
-                    Ver cambio domicilio
-                  </button>
-                @endif
-              </td>
-              <td>
-                {{Form::open(array('action' =>['AdminOrdersController@destroy',$order->order_id],
-                'method' => 'delete'))}}
-                  <button type="submit" class="btn btn-danger btn-xs">
-                    <span class="glyphicon glyphicon-remove"></span> Eliminar
-                  </button>
-                 {{Form::close()}}
-              </td>
-              </tr>
-            @endforeach
-          </tbody>
-      </table>
-    </div>
-    <center>
-      {{$orders->links()}}
-    </center>
-  @endif
-  @include('admin::address.partials.change_address')
+			<tbody>
+				@foreach($orders as $order)
+				<tr>
+					<td>
+						{{$order->user->gerencia}}
+					</td>
+					<td>
+						{{$order->user->ccosto}}
+					</td>
+					<td>
+						{{link_to_action('AdminOrdersController@show', $order->order_id, [$order->order_id])}}
+					</td>
+					<td>
+						{{$order->comments}}
+					</td>
+					<td>
+						{{$order->order_date}}
+					</td>
+					<td>
+						@if($order->status == 0)
+						Pendiente
+						@elseif($order->status==1)
+						Recibido <span class="glyphicon glyphicon-check"></span>
+						@elseif($order->status==2)
+						Recibido incompleto.
+						@if($complain = $order->order_complain)
+						<small>{{$complain->complain}}</small>
+						@endif
+						@endif
+					</td>
 
-       {{Form::close()}}
-@stop
+					<td>
 
-@section('script')
-  <script>
-    $(function(){
-      $('#cambio').click(function(){
-          var modal = $('#change-address').modal();
+						{{$order->user->address ? $order->user->address->domicilio : "N/A"}}
+						@if($order->user->address ? ($order->user->address->posible_cambio != null) : false )
+						<button data-id="{{$order->user->address->id}}" data-domicilio="{{$order->user->address->domicilio}}" data-posible-cambio="{{$order->user->address->posible_cambio}}" class="btn btn-primary btn-xs" id="cambio">
+							Ver cambio domicilio
+						</button>
+						@endif
+					</td>
+					<td>
+						{{Form::open(array('action' =>['AdminOrdersController@destroy',$order->order_id],
+						'method' => 'delete'))}}
+						<button type="submit" class="btn btn-danger btn-xs">
+							<span class="glyphicon glyphicon-remove"></span> Eliminar
+						</button>
+						{{Form::close()}}
+					</td>
+				</tr>
+				@endforeach
+			</tbody>
+		</table>
+	</div>
+	<center>
+		{{$orders->links()}}
+	</center>
+	@endif
+	@include('admin::address.partials.change_address')
 
-          $('#domicilio').text($(this).attr('data-domicilio'));
-          $('#posible_cambio').text($(this).attr('data-posible-cambio'));
-          var action = 'address/'+ $(this).attr('data-id');
-          $('#change-address-form').attr('action',action);          
-          modal.show();
-      });
+	{{Form::close()}}
+	@stop
+
+	@section('script')
+	<script>
+		$(function(){
+			$('#cambio').click(function(){
+				var modal = $('#change-address').modal();
+
+				$('#domicilio').text($(this).attr('data-domicilio'));
+				$('#posible_cambio').text($(this).attr('data-posible-cambio'));
+				var action = 'address/'+ $(this).attr('data-id');
+				$('#change-address-form').attr('action',action);          
+				modal.show();
+			});
 
 
-      $('.approve').click(function(){
-        console.log($(this).attr('data-value'));
-        if($(this).attr('data-value') == 1)
-          $('#valor_aprobado').val(1);
-        else
-          $('#valor_aprobado').val(0);
+			$('.approve').click(function(){
+				console.log($(this).attr('data-value'));
+				if($(this).attr('data-value') == 1)
+					$('#valor_aprobado').val(1);
+				else
+					$('#valor_aprobado').val(0);
 
-        $('#change-address-form').submit();
-      });
-    });
-  </script>
+				$('#change-address-form').submit();
+			});
+		});
+	</script>
 
-@endsection
+	@endsection
--- app/views/orders/index.blade.php
+++ app/views/orders/index.blade.php
@@ -4,79 +4,79 @@
 
 @if($orders->count() == 0)
 <div class="alert alert-info">
-  Usted no ha realizado pedidos todavía.
-  @if(Auth::user()->cart_products->count() > 0)
-  Haga click <a href="/carrito" class="alert-link">aquí</a> para revisar su carrito y enviar su orden.
-  @else
-  Haga click <a href="/productos" class="alert-link">aquí</a> para agregar productos a su carrito.
-  @endif
+	Usted no ha realizado pedidos todavía.
+	@if(Auth::user()->cart_products->count() > 0)
+	Haga click <a href="/carrito" class="alert-link">aquí</a> para revisar su carrito y enviar su orden.
+	@else
+	Haga click <a href="/productos" class="alert-link">aquí</a> para agregar productos a su carrito.
+	@endif
 </div>
 
 @else
 
 @if($errors->count() > 0)
 <div class="alert alert-danger">
-  {{$errors->first()}}
+	{{$errors->first()}}
 </div>
 @endif
 
 
 <table class="table table-striped">
-  <thead>
-    <tr>
-      <th>
-        No. pedido
-      </th>
+	<thead>
+		<tr>
+			<th>
+				No. pedido
+			</th>
 
-      <th>
-        Comentarios
-      </th>
-      <th>
-        Fecha de pedido
-      </th>
-      <th>
-        Estatus
-      </th>
-    
-    </tr>
-  </thead>
+			<th>
+				Comentarios
+			</th>
+			<th>
+				Fecha de pedido
+			</th>
+			<th>
+				Estatus
+			</th>
 
-  <tbody>
-    @foreach($orders as $order)
-    <tr>
-      <td>
-        {{link_to_action('OrdersController@show', $order->id, [$order->id])}}
-      </td>
-      <td>
-        {{$order->comments}}
-      </td>
-      <td>
-        {{$order->created_at->format('d-m-Y')}}
-      </td>
-      <td>
-        @if($order->status == 0)
-        Pendiente
-        @elseif($order->status==1)
-        Recibido <span class="glyphicon glyphicon-check"></span>
-        @elseif($order->status==2)
-        Recibido incompleto.
-          @if($complain = $order->order_complain)
-          <small>{{$complain->complain}}</small>
-          @endif
-        @endif
-      </td>
-      @if($order->status == 0)
-      <td>
-    {{Form::open(array('action' =>['OrdersController@destroy',$order->id],'method' => 'delete'))}}
-     <button class="btn btn-xs btn-danger" data-product-id="{{$order->id}}" >Eliminar</button>  
-     {{Form::close()}}
-       </td>
-    @endif 
-      </tr>
+		</tr>
+	</thead>
+
+	<tbody>
+		@foreach($orders as $order)
+		<tr>
+			<td>
+				{{link_to_action('OrdersController@show', $order->id, [$order->id])}}
+			</td>
+			<td>
+				{{$order->comments}}
+			</td>
+			<td>
+				{{$order->created_at->format('d-m-Y')}}
+			</td>
+			<td>
+				@if($order->status == 0)
+				Pendiente
+				@elseif($order->status==1)
+				Recibido <span class="glyphicon glyphicon-check"></span>
+				@elseif($order->status==2)
+				Recibido incompleto.
+				@if($complain = $order->order_complain)
+				<small>{{$complain->complain}}</small>
+				@endif
+				@endif
+			</td>
+			@if($order->status == 0)
+			<td>
+				{{Form::open(array('action' =>['OrdersController@destroy',$order->id],'method' => 'delete'))}}
+				<button class="btn btn-xs btn-danger" data-product-id="{{$order->id}}" >Eliminar</button>  
+				{{Form::close()}}
+			</td>
+			@endif 
+		</tr>
 
 
-    @endforeach
-  </tbody>
+		@endforeach
+	</tbody>
 </table>
 
 @endif
