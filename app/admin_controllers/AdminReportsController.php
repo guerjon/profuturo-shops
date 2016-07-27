4d4eb9ee6365e97db4f42a7a2141d2a2bad8591c
jonguer <jona_54_.com@hotmail.com>
2016-07-27 11:55:31 -0500

    otra prueba para el servidor

--- app/admin_controllers/AdminReportsController.php
+++ app/admin_controllers/AdminReportsController.php
@@ -157,8 +157,10 @@ class AdminReportsController extends AdminBaseController{
     if(Input::has('region_id')){
       $query->where('users.region_id',Input::get('region_id'));
     }
-
+    dd($query->get());
     Log::debug($query->get());
+
+
     if(Input::has('excel')){
 
         $headers = [   
