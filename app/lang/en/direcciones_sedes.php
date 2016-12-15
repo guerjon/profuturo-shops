0e97a192851f44f50e6f01b25b9c8dd9f5e31979
jonguer <jona_54_.com@hotmail.com>
2016-12-14 18:55:00 -0600

    ahora los usuarios capacitadores pueden seleccionar su sede en vez de los centros de costos

---
+++ app/lang/en/direcciones_sedes.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+	800001 => 'Av. Paseo Triunfo de la República #3340 Int 101 y 102, Col. Partido Romero, C.P. 32030 Cd. Juárez, Chihuahua',
+	800002 => 'C. JOSE CLEMENTE OROZCO #2410 COL: ZONA RIO C.P 22010  (2 salas de capacitacion)',
+	800003 => 'doctor coss 735, colonia centro mOnterrey',
+	800004 => 'AV CHAPULTEPEC #15 PISO 12 COLONIA LADRON DE GUEVARA, GUADALAJARA JALISCO 44600',
+	800005 => 'Bvld Adolfo Ruiz Cortinez L7 y 8 #2202 Fracc. Costa de Oro Boca del Rio Veracruz Ver. CP 94294 Edificio Bussines Center Veracruz',
+	800006 => '1 SALA  BLVD VASCO DE QUIROGA #101 PB COL LOS GAVILANES LEON GTO',
+	800007 => '2 SALAS BLVD BERNARDO QUINTANA 401 PISO 5 FRACC ALAMOS TERCERA SECCION',
+	800008 => 'INSURGENTES SUR 546 COL. ROMA SUR',
+	800009 => 'Av. Insurgentes sur 2047 PA San Angel Inn 01000',
+	800010 =>'HAMBURGO 206 PISO 3 JUAREZ 06600',
+	800011 =>'PRESA SALINILLAS 370 901 Y 903 IRRIGACION 11500',
+];
+
+
