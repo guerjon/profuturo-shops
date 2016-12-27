ecce45db52a1cdd41c034885c2eb6f44bba3b6d5
jonguer <jona_54_.com@hotmail.com>
2016-12-26 23:56:22 -0600

    cambios en la seccion de solicitudes generales, se corrigieron todos los errores

---
+++ app/lang/en/status.php
@@ -0,0 +1,17 @@
+<?php 
+
+	return [
+		0 => "Acabo de recibir tu solicitud, en breve me comunicare contigo",
+		1 => "En estos momentos estoy localizando los proveedores que pueden contar con el artículo que necesitas",
+		2 => "Me encuentro en espera de las cotizaciones por parte de los proveedores seleccionados",
+		3 => "Ya recibí las propuestas correspondientes, estoy en proceso de análisis de costo beneficio",
+		4 => "Te comparto el cuadro comparativo con las mejores ofertas de acuerdo a tu necesidad",
+		5 => "Recotizar",
+		6 => "Conforme a tu elección, ingresa tu solicitud en People Soft",
+		7 => "Ya se envió la orden de compra al proveedor",
+		8 => "La fecha de entrega de tu pedido es",
+		9 => "Tu pedido llego en excelentes condiciones, en el domicilio y recibió",
+		10 => "Fue un placer atenderte, me apoyarías con la siguiente encuesta de satisfacción",
+		11 => "La encuesta ha sido contestada",
+		12 => "Encuesta cancelada"					
+	];
